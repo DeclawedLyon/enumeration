25ecbc24779ee0b9c1fa42fda6a4fe76f4c72a6c
DeclawedLyon <declan.e.lyons@gmail.com>
2021-09-14 19:52:25 -0600

    Implemented filter functions

--- filters.rb
+++ filters.rb
@@ -3,10 +3,15 @@
 
 def find(id)
   # Your code Here
+  @candidates.forEach(candidate => {
+    if candidate.id === id
+    return candidate
+  }) else return null
 end
 
 def experienced?(candidate)
   # Your code Here
+  candidate.years_of_experience >= 2 ? true : false
 end
 
 def qualified_candidates(candidates)
--- main.rb
+++ main.rb
@@ -2,8 +2,17 @@
 # It requires the other files/gems that it needs
 require 'pp'
 require './candidates'
-require './filters'
+# require './filters'
 
 ## Your test code can go here
 
 pp @candidates
+
+a = [ "a", "b", "c" ]
+puts a.include?("b")
+
+def isInside?(arr ,val)
+  arr.include?(val) ? "yes" : "no"
+end
+
+puts isInside?(a, "b")
